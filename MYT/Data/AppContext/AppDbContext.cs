8ccc3bc56b19070eef112c12c2712d3dc775a5bd
Vitaliy <vetalkasmile@gmail.com>
2020-02-03 16:10:48 +0300

    Added MYT

---
+++ MYT/Data/AppContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MYT.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MYT.Data.AppContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<TodoItem> TodoItems { get; set; }
+        public virtual DbSet<TodoItemList> TodoItemLists { get; set; }
+    }
+}
---
+++ MYT/Data/BaseManger.cs
@@ -0,0 +1,19 @@
+﻿using MYT.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MYT.Data
+{
+    public class BaseManger
+    {
+        public DateTime TimeOfCreate { get; set; }
+        public int CreatorId { get; set; }
+        public DateTime LastChanged { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ApplicationUser UserId { get; set; }
+    }
+}
